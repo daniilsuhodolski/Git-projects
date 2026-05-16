488e1c5ef0e7713fa70eefcddc477586f7aab6df
daniilsuhodolski <daniilsuhodolski@gmail.com>
2026-05-16 17:03:22 +0300

    Работа с пвсевдокодом

--- main.cpp
+++ main.cpp
@@ -1,5 +1,15 @@
 #include <iostream>
 using namespace std;
 int main() {
-    cout << "Daniil";
-}
+    int N=10;
+    int a[10]={4,5,0,2,6,1,7,9,0,1};
+    bool p=true;
+    for (int j=0; j<10-2; j++) {
+        if(A[j] > A[j + 1]) {
+            int buf = a [j];
+            a[j] = a[j + 1];
+            a[j + 1] = buf;
+            p=false;
+        }
+    }
+    N = N - 1;
